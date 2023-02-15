5da913b059301e82a40ea96dc87e0b9ea3cacf8c
N-studies <122186098+N-studies@users.noreply.github.com>
2023-02-15 15:47:21 -0500

    make 4x4 grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,22 @@
 // task 1 make a div change color while hovered over
 const div = document.querySelector(".js");
 div.addEventListener("mousemove", () => {
-    div.style.cssText = "background-color: black; color: white;"
-})
+  div.style.cssText = "background-color: black; color: white;";
+});
+
+const grid = document.querySelector(".container");
+
+  for (let j = 0; j < 4; j++) {
+    for (let i = 0; i < 4; i++) {
+    const divs = document.createElement("div");
+    divs.classList.add("js");
+    grid.appendChild(divs);
+    divs.addEventListener("mousemove", () => {
+        divs.style.cssText = "background-color: black; color: white;";
+    });
+    }
+  }
+/*
+divs.addEventListener("mousemove", () => {
+    divs.style.cssText = "background-color: black; color: white;";
+  });*/
