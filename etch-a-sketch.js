19cc14263087a01a89b7ae9c03b907cf60cdeaa0
N-studies <122186098+N-studies@users.noreply.github.com>
2023-02-17 18:13:16 -0500

    Finish assignment

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,29 +1,57 @@
-// task 1 make a div change color while hovered over
-/*const div = document.querySelector(".js");
-div.addEventListener("mousemove", () => {
-  div.style.cssText = "background-color: black; color: white;";
-});*/
+function createGrid(unitsPerSide) {
+  const grid = document.querySelector(".container");
+  const squareSize = 500 / unitsPerSide - 2;
+  for (let j = 0; j < unitsPerSide; j++) {
+    for (let i = 0; i < unitsPerSide; i++) {
+      const divs = document.createElement("div");
+      divs.style.cssText = `border: 1px solid black; height: ${squareSize}px; width: ${squareSize}px;`;
+      divs.classList.add("js");
+      grid.appendChild(divs);
+    }
+  }
+  
+    const alldiv = grid.querySelectorAll("div");
+    alldiv.forEach((div) => {
+      div.addEventListener("mousemove", () => {
+        div.setAttribute("style", `background-color: black; border: 1px solid black; height: ${squareSize}px; width: ${squareSize}px;`);
+  });
+});
+}
+createGrid(16);
 
-const grid = document.querySelector(".container");
-
-for (let j = 0; j < 100; j++) {
-  for (let i = 0; i < 100; i++) {
-    const divs = document.createElement("div");
-    divs.classList.add("js");
-    grid.appendChild(divs);
-    divs.addEventListener("mousemove", () => {
-      divs.style.cssText = "background-color: black;";
-    });
+function deleteGrid() {
+  const grid = document.querySelector(".container");
+  while (grid.firstChild){
+    grid.removeChild(grid.firstChild);
   }
 }
 
+function resetGrid(size) {
+  deleteGrid()
+  createGrid(size)
+}
 
 const btn = document.querySelector(".size");
 btn.addEventListener("click", () => {
- /* divs = document.querySelectorAll("div");
+  const units = prompt("How many squares per side?");
+  resetGrid(units);
+})
+
+
+
+
+/*
+function fill() {
+  divs.style.cssText = "background-color: black;";
+};*/
+
+/*
+const btn = document.querySelector(".size");
+btn.addEventListener("click", () => {
+  divs = document.querySelectorAll("div");
   divs.forEach((div) => {
     div.classList.remove("js");
-  }); */
+  }); 
   //Need to remove previous children!
   
   while (grid.firstChild){
@@ -31,7 +59,7 @@ btn.addEventListener("click", () => {
   }
   console.log(grid);
   
-  /*--------------------------- */
+  
   const numSides = prompt("enter amount of boxes per side");
   const squareSideLength = 500 / numSides - 2; // this takes border into account
   
@@ -55,11 +83,11 @@ btn.addEventListener("click", () => {
     }
     
   }
- 
-  
-  /*divs2.addEventListener("mousemove", () => {
+ */
+
+/*divs2.addEventListener("mousemove", () => {
         divs2.style.cssText = "background-color: black;";
-      });;*/
-});
+      });;
+});*/
 
 // border-top: 2px solid black; border-left: 2px solid black;
